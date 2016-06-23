bd77ec12c05ac0b01bb296df076b2a61874c507e
Juan Manuel Carucci <juanmanuelcarucci@gmail.com>
2016-06-23 20:05:43 -0300

    busqueda de puntos de interes

--- poi/src/main/java/poi/search/search.java
+++ poi/src/main/java/poi/search/search.java
@@ -1,14 +1,18 @@
 package poi.search;
 
 import java.time.DayOfWeek;
-
 import org.joda.time.*;
-
 import poi.constant.Service;
+import java.util.Scanner;
 
 public class search {
-	public static void main(String[] args) 
-	{	
-		System.out.println("Enter the point of interest you want to search:");
-	}
+		public void searchPOI(){
+			Scanner in = new Scanner(System.in);
+			String POI;
+			
+			System.out.println("Enter the point of interest you want to search:");
+			POI = in.next();
+			
+			
+		}
 }
