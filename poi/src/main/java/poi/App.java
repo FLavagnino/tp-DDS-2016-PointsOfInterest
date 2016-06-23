7b98d6fa3a0b6e3ed25e40060a40bc7e8563ce83
Juan Manuel Carucci <juanmanuelcarucci@MacBook-Pro-de-Juan-2.local>
2016-06-23 19:14:09 -0300

    prueba

--- poi/src/main/java/poi/App.java
+++ poi/src/main/java/poi/App.java
@@ -4,6 +4,6 @@ public class App
 {
 	public static void main(String[] args) 
 	{		
-		System.out.println("Las pruebas las ejecutamos con jUnit, esta clase no hace nada.");
+		System.out.println("Las pruebas las ejecutamos con jUnit, esta clase no hace nada...");
 	}
 }
