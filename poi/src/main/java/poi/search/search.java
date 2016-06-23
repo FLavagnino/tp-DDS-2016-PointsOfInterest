ff8edce7562db475f9682aa27485fdb3dba387bd
Juan Manuel Carucci <juanmanuelcarucci@gmail.com>
2016-06-23 19:47:17 -0300

    busqueda de puntos de interes

---
+++ poi/src/main/java/poi/search/search.java
@@ -0,0 +1,14 @@
+package poi.search;
+
+import java.time.DayOfWeek;
+
+import org.joda.time.*;
+
+import poi.constant.Service;
+
+public class search {
+	public static void main(String[] args) 
+	{	
+		System.out.println("Enter the point of interest you want to search:");
+	}
+}
