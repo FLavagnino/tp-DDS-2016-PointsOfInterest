42d511eb8366f1e601e0d2eb52cf461a609abb6c
Juan Manuel Carucci <juanmanuelcarucci@gmail.com>
2016-08-29 16:29:47 -0300

    asd

--- poi/src/main/java/ar/edu/utn/dds/poi/users/terminal.java
+++ poi/src/main/java/ar/edu/utn/dds/poi/users/terminal.java
@@ -6,23 +6,6 @@ import org.joda.time.DateTime;
 
 public class terminal {
 	
-	String name;
-	String password;
-	
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	public String getName() 
-	{
-		return name;
-	}
-	public void setPassword(String password)
-	{
-		this.password = password;
-	}
-	public String getPassword(){
-		return password;
-	}
+
 	
 }
