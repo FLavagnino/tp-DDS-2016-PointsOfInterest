67f13d5ba716f1f1552f0f808b651ea674e254e1
Juan Manuel Carucci <juanmanuelcarucci@gmail.com>
2016-08-30 16:57:32 -0300

    agrego clase para reportes

---
+++ poi/src/main/java/ar/edu/utn/dds/poi/service/reportData.java
@@ -0,0 +1,17 @@
+package ar.edu.utn.dds.poi.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class reportData {
+
+	private Integer date;
+	private Integer numberOfSearches;
+	
+	public numberOfSearchesPerDay(Integer date, Integer numberOfSearches){
+		this.date = date;
+		this.numberOfSearches = numberOfSearches;
+		//FALTA TERMINARLO.
+	}
+	
+}
