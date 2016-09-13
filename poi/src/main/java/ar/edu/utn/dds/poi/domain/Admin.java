62cac94c6fc942c533f4a153387aab01e029f0b1
Juan Manuel Carucci <juanmanuelcarucci@gmail.com>
2016-09-13 17:07:58 -0300

    se agregan algunas cosas a seguir de la entrega 4

--- poi/src/main/java/ar/edu/utn/dds/poi/domain/Admin.java
+++ poi/src/main/java/ar/edu/utn/dds/poi/domain/Admin.java
@@ -1,5 +1,16 @@
 package ar.edu.utn.dds.poi.domain;
+import java.util.*;
 
 public class Admin extends User 
 {
+	private ArrayList<String> ListOfProcess = new ArrayList<String>();
+	
+	public void setProcessList(ArrayList<String> List){
+		this.ListOfProcess = List;
+	}
+	
+	public ArrayList<String> getListOfProcess(){
+		return ListOfProcess;
+	}
+		
 }
---
+++ poi/src/main/java/ar/edu/utn/dds/poi/domain/Process.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.dds.poi.domain;
+
+public class Process {
+
+	public void updateShop(){
+		//hacer
+	}
+	
+	public void deletePOI(){
+		//hacer
+	}
+	public void addActionToUsers(){
+		//hacer
+	}
+	public void definitionOfAMultipleProcess(){
+		//hacer
+	}
+	
+}
